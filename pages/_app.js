1f9aefeae4994c2c024a3e61df2d9ccdebd34d21
Sanjai R <62475598+Sanjai-R@users.noreply.github.com>
2021-07-26 00:59:59 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import 'tailwindcss/tailwind.css'
-import "../styles/globals.css";
+import "../Styles/globals.css";
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
